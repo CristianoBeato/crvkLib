f28a4d5723fab570e4a8149e7a2049b558210594
Beato <cristianobeato_dm@hotmail.com>
2025-07-23 12:31:53 -0300

    Update documantation header

--- lib/crvkCore.hpp
+++ lib/crvkCore.hpp
@@ -1,17 +1,23 @@
-// ===============================================================================================
-// crvkCore - Vulkan + SDL minimal framework
-// Copyright (c) 2025 Beato
-//
-// This file is part of the crvkCore library and is licensed under the
-// MIT License with Attribution Requirement.
-//
-// You are free to use, modify, and distribute this file (even commercially),
-// as long as you give credit to the original author:
-//
-//     “Based on crvkCore by Beato – https://github.com/seuusuario/crvkCore”
-//
-// For full license terms, see the LICENSE file in the root of this repository.
-// ===============================================================================================
+/*
+===========================================================================================
+    This file is part of crvkLib Vulkan + SDL minimal framework.
+
+    Copyright (c) 2025 Cristiano B. Santos <cristianobeato_dm@hotmail.com>
+    Contributor(s): none yet.
+
+-------------------------------------------------------------------------------------------
+
+ This file is part of the crvkLib library and is licensed under the
+ MIT License with Attribution Requirement.
+
+ You are free to use, modify, and distribute this file (even commercially),
+ as long as you give credit to the original author:
+
+     “Based on crvkCore by Cristiano Beato – https://github.com/CristianoBeato/crvkLib”
+
+ For full license terms, see the LICENSE file in the root of this repository.
+===============================================================================================
+*/
 
 #ifndef __CRVK_IMPLEMENTATION_HPP__
 #define __CRVK_IMPLEMENTATION_HPP__
